f696964e8654ce1fc14f72f8805f9189bee51866
Eun Chul Lee <ecl009@ucsd.edu>
2015-02-10 17:10:07 -0800

    fixed javascript on addTask

--- public/js/newTask.js
+++ public/js/newTask.js
@@ -1,7 +1,27 @@
-  $('#setTimeCheckBox').live('change', function(){
-      if ( $(this).is(':checked') ) {
-         $('#date').show();
-     } else {
-         $('#date').hide();
-     }
- });
+ 'use strict';
+ $(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+	console.log("Javascript connected!");
+	$('.setTimeCheckBox').hide();
+	$('.setLocationCheckBox').hide();
+	$('.dateRepeatChecked').hide();
+	$('.setLocationList').hide();
+  $('#setTimeCheck').click(function() {
+	$('.setTimeCheckBox')[this.checked ? "show" : "hide"]();	
+  });
+  
+  $('#setLocation').click(function() {
+	$('.setLocationCheckBox')[this.checked ? "show" : "hide"]();	
+  });
+  
+  $('.repeated').click(function() {
+	$('.dateRepeatChecked')[this.checked ? "show" : "hide"]();	
+  });
+  
+  $('.setLocation').click(function() {
+	$('.setLocationList')[this.checked ? "show" : "hide"]();
+  });
+}
